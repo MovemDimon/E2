a74fe2b939c3ef4b06d389f1241eb02189baa975
MovemDimon <movem9013@gmail.com>
2025-05-03 13:17:19 +0330

    Update cojs.js

--- material/js/cojs.js
+++ material/js/cojs.js
@@ -1,14 +1,9 @@
-
-document.getElementById('copyButton').addEventListener('click', function() {
-
-  var copyText = document.getElementById('link');
-
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); 
-  
-
-  document.execCommand('copy');
-  
- 
- 
-});
+// Cojs.js (copy link)
+export function setupCopy() {
+  document.getElementById('copyButton').addEventListener('click', () => {
+    const copyText = document.getElementById('link');
+    copyText.select();
+    document.execCommand('copy');
+    alert('Link copied to clipboard!');
+  });
+}
