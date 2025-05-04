6a9a5464e678f3d48b92ba4140d96e38b955d459
MovemDimon <movem9013@gmail.com>
2025-05-04 16:28:03 +0330

    Update config.js

--- material/js/config.js
+++ material/js/config.js
@@ -1,5 +1,10 @@
 // config.js
-export const CONFIG = {
-  API_BASE_URL: 'https://httpbin.org',
-  WS_HOSTS: ['wss://echo.websocket.events/ws']
+window.CONFIG = {
+  // جایگزین URL واقعی خود کنید
+  API_BASE_URL: 'https://YOUR_API_ENDPOINT',
+  WS_HOSTS: [
+    'wss://ws-frankfurt.fly.dev/ws',
+    'wss://ws-milan.fly.dev/ws',
+    'wss://ws-amsterdam.fly.dev/ws'
+  ]
 };
