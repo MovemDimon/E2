da2fadfb5fcf329c2423a2223505361dc7a8c0f8
MovemDimon <movem9013@gmail.com>
2025-05-17 11:41:27 +0330

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,14 @@ module.exports = {
     '/node_modules/',
     '/cypress/'
   ],
+  
+  const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    entry: path.resolve(__dirname, 'material/js/main.js')
+  }
+};
 
   // ← این قسمت را قرار دهید:
   roots: [
