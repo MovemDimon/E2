91a6c7829aa6d34ab8d36d227cd97528bb40771b
MovemDimon <movem9013@gmail.com>
2025-05-17 15:33:20 +0330

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:8080',
-    specPattern: 'cypress/integration/**/*.spec.js',
-    supportFile: false
-  }
-});
