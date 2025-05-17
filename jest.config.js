0a5341ffbc8bf50c8aa102b19611e911e030b27b
MovemDimon <movem9013@gmail.com>
2025-05-17 11:46:40 +0330

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,21 +11,12 @@ module.exports = {
     '/node_modules/',
     '/cypress/'
   ],
-  
-  const path = require('path');
 
-module.exports = {
-  configureWebpack: {
-    entry: path.resolve(__dirname, 'material/js/main.js')
-  }
-};
-
-  // ← این قسمت را قرار دهید:
+  // مسیرهای جستجوی تست
   roots: [
     '<rootDir>'
   ],
   testMatch: [
-    // هر فایلی با پسوند .test.js یا .spec.js در ریشه یا زیرپوشه‌ها
     '<rootDir>/**/*.test.js',
     '<rootDir>/**/*.spec.js'
   ]
