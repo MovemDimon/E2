dcdccb7452d12712cdfda538d19f6b48d0d3f723
MovemDimon <movem9013@gmail.com>
2025-05-19 20:10:50 +0330

    Update js.js

--- material/js/js.js
+++ material/js/js.js
@@ -18,7 +18,7 @@ function updateBalance() {
  * @param {number} reward - Reward amount
  * @param {string} url - URL to open
  */
-function completeTaskUrl(taskUrlKey, reward, url) {
+function completeTaskUrl(reward, taskUrl) {
   const today = new Date().toISOString().split('T')[0];
 
   if (localStorage.getItem(taskUrlKey) === today) {
